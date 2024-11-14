b61327dc9adedad1be411d958f734089e084691d
Gaspard Malmon <malmon.gaspard@gmail.com>
2024-11-14 13:47:09 +0100

    ++

---
+++ assets/Tiled_asset/Tiles paper adv.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Tiles paper adv" tilewidth="16" tileheight="16" tilecount="369" columns="9">
+ <image source="../Vrac/Paper-Pixels-8x8/Tiles.png" width="144" height="656"/>
+</tileset>
