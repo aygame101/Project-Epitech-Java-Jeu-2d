687320f1e2d5dc912b4e5a18146fc53c42dac398
Gaspard Malmon <malmon.gaspard@gmail.com>
2024-11-21 14:44:51 +0100

    test coins

--- assets/Tiled_asset/morning_adventures_tileset_16x16.tsx
+++ assets/Tiled_asset/morning_adventures_tileset_16x16.tsx
@@ -1,4 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="morning_adventures_tileset_16x16" tilewidth="16" tileheight="16" tilecount="60" columns="10">
  <image source="../Vrac/morning_adventures_tilemap/morning_adventures_tileset_16x16.png" width="160" height="96"/>
+ <tile id="57">
+  <animation>
+   <frame tileid="48" duration="100"/>
+   <frame tileid="49" duration="100"/>
+   <frame tileid="59" duration="100"/>
+   <frame tileid="57" duration="100"/>
+   <frame tileid="58" duration="100"/>
+  </animation>
+ </tile>
 </tileset>
