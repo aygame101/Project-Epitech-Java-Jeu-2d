b5b938397140cd7b84fca9ed38f6e82e9b1fc20c
Gaspard Malmon <malmon.gaspard@gmail.com>
2024-11-23 23:25:38 +0100

    fin en gros

--- core/src/main/java/com/badlogic/drop/VictoryScreen.java
+++ core/src/main/java/com/badlogic/drop/VictoryScreen.java
@@ -8,7 +8,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class VictoryScreen implements Screen {
-    private final Main game; // Remplacez MyGame par le nom de votre classe principale
+    private final Main game;
     private Texture victoryImage;
     private SpriteBatch spriteBatch;
 
@@ -16,24 +16,22 @@ public class VictoryScreen implements Screen {
         this.game = game;
 
         // Charger l'image de victoire
-        victoryImage = new Texture(Gdx.files.internal("gameover.png")); // Assurez-vous que le fichier est dans le bon répertoire
+        victoryImage = new Texture(Gdx.files.internal("Victory.png"));
         spriteBatch = new SpriteBatch();
     }
 
     @Override
     public void show() {
-        // Méthode appelée lorsque l'écran est affiché
     }
 
     @Override
     public void render(float delta) {
-        // Effacer l'écran avec une couleur de fond
-        Gdx.gl.glClearColor(1, 1, 1, 1); // Couleur de fond blanche
+        Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         // Dessiner l'image de victoire
         spriteBatch.begin();
-        spriteBatch.draw(victoryImage, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()); // Dessiner l'image en plein écran
+        spriteBatch.draw(victoryImage, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         spriteBatch.end();
     }
 
@@ -60,6 +58,6 @@ public class VictoryScreen implements Screen {
     @Override
     public void dispose() {
         spriteBatch.dispose();
-        victoryImage.dispose(); // Libérer la mémoire de l'image
+        victoryImage.dispose();
     }
 }
